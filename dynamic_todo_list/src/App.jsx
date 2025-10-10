192a3bdf0554dd179f050ff1616d91976e42f14a
Deodatus Bijengsi <124217738+Bichesq@users.noreply.github.com>
2025-10-10 03:34:10 +0000

    short break

--- dynamic_todo_list/src/App.jsx
+++ dynamic_todo_list/src/App.jsx
@@ -1,19 +1,54 @@
 import { useState } from 'react'
+import ListItem from './components/ListItem.jsx'
 
 import './App.css'
 
 function App() {
-  const [todoItem, setTodoItem] = useState('');
-  const [category, setCategory] = useState('');
-  const 
+  const [data, setData] = useState({
+    todoItem: '',
+    category: ''
+  })
+  const [dataList, setDataList] = useState([])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setDataList((prev) => [
+       ...prev, data
+    ])
+    setData({
+      todoItem: "",
+      category: ""
+    })
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    
+    setData((prev) => (
+      {...prev, [name]: value}
+    ))
+  }
+
+  const handleDelete = () => {
+    setDataList(dataList.filter())
+  }
+
   return(
     <>
       <h1>Dynamic To Do List</h1>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" name='todoItem' value={todoItem} onChange={handleChange} placeholder='Enter Todo Item' />
-        <input type='text' name='category' value={category} onChange={handleChange} placeholder='Enter Category' />
+        <input type="text" name='todoItem' value={data.todoItem} onChange={handleChange} placeholder='Enter Todo Item' />
+        <br/>
+        <input type='text' name='category' value={data.category} onChange={handleChange} placeholder='Enter Category' />
+        <br/>
+        <br/>
+        <button type='submit' >Add Item</button>
       </form>
+      <ul>
+        {dataList.map((item, index) => <li key={index}><ListItem item={item.todoItem} category={item.category} /></li>) }
+      </ul>
+      
     </>
   )
 }
--- dynamic_todo_list/src/components/ListItem.jsx
+++ dynamic_todo_list/src/components/ListItem.jsx
@@ -1,7 +1,10 @@
 const ListItem = ({ item, category}) => {
+    
     return (
         <>
-            <div style={{display: flex, alignContent: "space-between"()}}><p>{item}</p> <p>category: {category}</p></div> <button>delete</button><button>update</button>
+            <div style={{display: "flex", alignContent: "space-between"}}><p>{item}  </p> <p>  category: {category}</p></div> <button onClick={handleDelete}>delete</button><button onClick={handleEdit}>update</button>
         </>
     )
 }
+
+export default ListItem;
